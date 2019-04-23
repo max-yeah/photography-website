218523b780484317c97e66833c7b331d67f010b5
caiwenjing <cai_wenjing@outlook.com>
2019-04-23 23:12:39 +0800

    DFBFDBD

--- photo/history_order.py
+++ photo/history_order.py
@@ -8,20 +8,20 @@ from photo.db import get_db
 
 bp = Blueprint('history_order', __name__)
 
-@bp.route('/history_order/index')
-def index():
+@bp.route('/history_order/history_index')
+def history_index():
     if (g.user):
-        g.current = "index"
+        g.current = "history_order.history_index"
         db = get_db()
         cursor = db.cursor()
         cursor.execute(
             "SELECT *"
             " FROM porder"
-            " WHERE status = complete"
+            " WHERE status = 'complete'"
             " ORDER BY satisfaction"
         )
         orders = cursor.fetchall()
-        return render_template('history_order/index.html', orders=orders)
+        return render_template('history_order/history_index.html', orders=orders)
     else:
         return redirect(url_for('auth.login'))
 
